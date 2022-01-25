e54a4674b0ef9c716010b7ca5cbd5f29cb31bb6c
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-01-25 14:47:46 +0700

    Create Service component

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -42,7 +42,7 @@ const Welcome = () => {
           </p>
           {
             !currentAccount && (
-              <button type='button' onClick={connectWallet} className='flex flex-row jutify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+              <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
                 <p className='text-white text-base font-semibold'>
                   Connect Wallet
                 </p>
